dbdb9211e4ad747c22fba72b3f0babdf9412ac48
SW1073 <eloiimerino@gmail.com>
2023-03-07 23:56:55 +0100

    user code para testear todas las funcionalidades

--- lab/zeos/user.c
+++ lab/zeos/user.c
@@ -17,21 +17,29 @@ int __attribute__ ((__section__(".text.main")))
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
      /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */
     
-    // Trigger a page fault
-    char *p = 0;
-    *p = 'x';
-
     // Test write syscall
     if (write(1, "\nHola que tal\n", 14) > 0) {
         write(1, "W\n", 2);
     }
     // Test both gettime and write syscalls
+    // Also test write() scrolling capabilities
     char *buffer = "\0\0\0\0\0\0\0\0\0\n";
     itoa(gettime(), buffer);
     write(1, buffer, 10);
+
     while(1) {
-        for (int i = 0; i < 1500000000; ++i);
+        for (int i = 0; i < 50000000; ++i);
         itoa(gettime(), buffer);
         write(1, buffer, 10);
     }
+
+    // Trigger a page fault
+    char *p = 0;
+    *p = 'x';
+
+    // loop forever.
+    // This point shall never be reached, since the page
+    // fault exception never returns
+    write(1, "This point shall never be reached.", 34);
+    while(1);
 }
