2e90b491c1bbef34a89dc46da6778510fc37ed0b
SW1073 <eloiimerino@gmail.com>
2023-03-16 16:17:56 +0100

    Modificado el task_struct con la entrada de lista

--- lab/zeos/include/sched.h
+++ lab/zeos/include/sched.h
@@ -17,6 +17,7 @@ enum state_t { ST_RUN, ST_READY, ST_BLOCKED };
 struct task_struct {
   int PID;			/* Process ID. This MUST be the first field of the struct. */
   page_table_entry * dir_pages_baseAddr;
+  struct list_head list; // Entrada para las colas.
 };
 
 union task_union {
