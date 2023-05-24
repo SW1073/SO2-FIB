398a6b161059dbc0bc47c7bd280b532836e8ef05
SW1073 <eloiimerino@gmail.com>
2023-05-24 22:59:38 +0200

    Primera version del mapa

--- lab/zeos/user.c
+++ lab/zeos/user.c
@@ -65,6 +65,59 @@ void func(int i) {
     exit_thread();
 }
 
+#define HEIGHT 21
+#define WIDTH 80
+
+char mapa_bmp[HEIGHT * WIDTH / 8] = {
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 
+    0xff, 0xff, 0xff, 0xfe, 0x7f, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0x7f, 0xff, 
+    0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe, 0x00, 0x03, 0xff, 0x00, 0x7f, 0xc0, 0x1f, 0xfc, 
+    0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 0x7f, 0xc0, 0x1f, 0xfc, 0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 
+    0x7f, 0xc0, 0x1f, 0xfc, 0x00, 0x00, 0x00, 0x03, 0xff, 0x00, 0x7f, 0xc0, 0x1f, 0xfc, 0x00, 0x00, 
+    0x00, 0x03, 0xc3, 0x00, 0x7f, 0xc0, 0x18, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0x00, 0x7f, 0xc0, 
+    0x18, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0x00, 0x7f, 0xc0, 0x18, 0x3c, 0x00, 0x00, 0x00, 0x03, 
+    0xc3, 0x00, 0x7f, 0xc0, 0x18, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0x00, 0x7f, 0xc0, 0x18, 0x3c, 
+    0x00, 0x00, 0x00, 0x03, 0xc3, 0x00, 0x7f, 0xc0, 0x18, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0x00, 
+    0x7f, 0xc0, 0x18, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0xff, 0xff, 0xff, 0xf8, 0x3c, 0x00, 0x00, 
+    0x00, 0x03, 0xc3, 0xff, 0xc0, 0x7f, 0xf8, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0xff, 0xc0, 0x7f, 
+    0xf8, 0x3c, 0x00, 0x00, 0x00, 0x03, 0xc3, 0xff, 0xc0, 0x7f, 0xf8, 0x3c, 0x00, 0x00, 0x00, 0x03, 
+    0xc3, 0xff, 0xc0, 0x7f, 0xf8, 0x3c, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00
+};
+
+char line_buffer[WIDTH+12] = "\[0;03H\[46;8mXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+void draw_map() {
+    unsigned char mask, jj, ii;
+    for (int i = 0; i < HEIGHT; i++) {
+        jj = 0;
+        for (int j = 0; j < WIDTH/8; j++) {
+            mask = 0x80;
+            while (mask != 0) {
+                if (!(mapa_bmp[i*(WIDTH/8) + j] & mask))
+                    // Draw a wall
+                    line_buffer[12+jj] = 'X';
+                else
+                    // Draw a space
+                    line_buffer[12+jj] = '\0';
+                mask >>= 1;
+                jj++;
+            }
+        }
+        ii = i + 3;
+        char c[2] = {'\0', '\0'};
+        itoa(ii, c);
+        if (ii < 10) {
+            line_buffer[3] = '0';
+            line_buffer[4] = c[0];
+        }
+        else {
+            line_buffer[3] = c[0];
+            line_buffer[4] = c[1];
+        }
+        write(1, line_buffer, WIDTH+12);
+    }
+}
 
     int __attribute__ ((__section__(".text.main")))
 main(void)
@@ -72,46 +125,33 @@ main(void)
     /* Next line, tries to move value 0 to CR3 register. This register is a privileged one, and so it will raise an exception */
     /* __asm__ __volatile__ ("mov %0, %%cr3"::"r" (0) ); */    
 
-    // Test write syscall
-    if (write(1, "\nHola que tal\n", 14) > 0) {
-        write(1, "W\n", 2);
-    }
-
-    mutex_init(&mutex);
-
-    create_thread((void*)func, 0);
-
-    mutex_lock(&mutex);
-    pid = 10;
-    mutex_unlock(&mutex);
-
-    // write_wrapper("\[H\[5;34;45mJola\[05;30;47mJola");
+    write_wrapper("\[0;1H Score: ");
+    write_wrapper("\[0;10H 9");
 
     // 20x60
-    write_wrapper("\[10;2H\[2;46;8m                                                             ");
-    write_wrapper("\[10;3H\[46;8m ");write_wrapper("\[70;3H\[46;8m ");
-    write_wrapper("\[10;4H\[46;8m ");write_wrapper("\[70;4H\[46;8m ");
-    write_wrapper("\[10;5H\[46;8m ");write_wrapper("\[70;5H\[46;8m ");
-    write_wrapper("\[10;6H\[46;8m ");write_wrapper("\[70;6H\[46;8m ");
-    write_wrapper("\[10;7H\[46;8m ");write_wrapper("\[70;7H\[46;8m ");
-    write_wrapper("\[10;8H\[46;8m ");write_wrapper("\[70;8H\[46;8m ");
-    write_wrapper("\[10;9H\[46;8m ");write_wrapper("\[70;9H\[46;8m ");
-    write_wrapper("\[10;10H\[46;8m ");write_wrapper("\[70;10H\[46;8m ");
-    write_wrapper("\[10;11H\[46;8m ");write_wrapper("\[70;11H\[46;8m ");
-    write_wrapper("\[10;12H\[46;8m ");write_wrapper("\[70;12H\[46;8m ");
-    write_wrapper("\[10;13H\[46;8m ");write_wrapper("\[70;13H\[46;8m ");
-    write_wrapper("\[10;14H\[46;8m ");write_wrapper("\[70;14H\[46;8m ");
-    write_wrapper("\[10;15H\[46;8m ");write_wrapper("\[70;15H\[46;8m ");
-    write_wrapper("\[10;16H\[46;8m ");write_wrapper("\[70;16H\[46;8m ");
-    write_wrapper("\[10;17H\[46;8m ");write_wrapper("\[70;17H\[46;8m ");
-    write_wrapper("\[10;18H\[46;8m ");write_wrapper("\[70;18H\[46;8m ");
-    write_wrapper("\[10;19H\[46;8m ");write_wrapper("\[70;19H\[46;8m ");
-    write_wrapper("\[10;20H\[46;8m ");write_wrapper("\[70;20H\[46;8m ");
-    write_wrapper("\[10;21H\[46;8m ");write_wrapper("\[70;21H\[46;8m ");
-    write_wrapper("\[10;22H\[2;46;8m                                                             ");
+    // write_wrapper("\[10;2H\[2;46;8m                                                             ");
+    // write_wrapper("\[10;3H\[46;8m ");write_wrapper("\[70;3H\[46;8m ");
+    // write_wrapper("\[10;4H\[46;8m ");write_wrapper("\[70;4H\[46;8m ");
+    // write_wrapper("\[10;5H\[46;8m ");write_wrapper("\[70;5H\[46;8m ");
+    // write_wrapper("\[10;6H\[46;8m ");write_wrapper("\[70;6H\[46;8m ");
+    // write_wrapper("\[10;7H\[46;8m ");write_wrapper("\[70;7H\[46;8m ");
+    // write_wrapper("\[10;8H\[46;8m ");write_wrapper("\[70;8H\[46;8m ");
+    // write_wrapper("\[10;9H\[46;8m ");write_wrapper("\[70;9H\[46;8m ");
+    // write_wrapper("\[10;10H\[46;8m ");write_wrapper("\[70;10H\[46;8m ");
+    // write_wrapper("\[10;11H\[46;8m ");write_wrapper("\[70;11H\[46;8m ");
+    // write_wrapper("\[10;12H\[46;8m ");write_wrapper("\[70;12H\[46;8m ");
+    // write_wrapper("\[10;13H\[46;8m ");write_wrapper("\[70;13H\[46;8m ");
+    // write_wrapper("\[10;14H\[46;8m ");write_wrapper("\[70;14H\[46;8m ");
+    // write_wrapper("\[10;15H\[46;8m ");write_wrapper("\[70;15H\[46;8m ");
+    // write_wrapper("\[10;16H\[46;8m ");write_wrapper("\[70;16H\[46;8m ");
+    // write_wrapper("\[10;17H\[46;8m ");write_wrapper("\[70;17H\[46;8m ");
+    // write_wrapper("\[10;18H\[46;8m ");write_wrapper("\[70;18H\[46;8m ");
+    // write_wrapper("\[10;19H\[46;8m ");write_wrapper("\[70;19H\[46;8m ");
+    // write_wrapper("\[10;20H\[46;8m ");write_wrapper("\[70;20H\[46;8m ");
+    // write_wrapper("\[10;21H\[46;8m ");write_wrapper("\[70;21H\[46;8m ");
+    // write_wrapper("\[10;22H\[2;46;8m                                                             ");
 
-    write_wrapper("\[0;0H\[2;44m Soy dim");
-    write_wrapper("\[0;1H\[1;44m Soy light");
+    draw_map();
 
     while (gettime() > 1000);
 
